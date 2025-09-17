7e9245c9ca3b1625fb6c2ac05e474fef7bbc9b82
JuanFigueredoGuardia <juanfigueredoguardia@gmail.com>
2025-09-17 12:04:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,30 +12,25 @@ const productos = [
 
     ["Black Sheep Dual (30.000 puff)", 35000, "imagenes/Black Sheep Dual.jpg", "descartables"],
 
-    ["Ignite (25.000 puff)", 25000, "imagenes/Ignite .jpg", "descartables"],
     ["Ignite gold (25.000 puff)", 27000, "imagenes/Ignite gold.jpg", "descartables"],
     ["Frosty (25.000 puff)", 19500, "imagenes/Frosty1.jpg", "descartables"],
 
-    ["Lost Mary (20.000 puff)", 23000, "imagenes/Lost Mary.jpg", "descartables"],
-    ["Space Prism (20.000 puff)", 20000, "imagenes/Space Prism.jpg", "descartables"],
-    ["Spaceman (20.000 puff)", 22000, "imagenes/Spaceman.jpg", "descartables"],
+    ["Space Prism (20.000 puff)", 22000, "imagenes/Spaceman.jpg", "descartables"],
     ["Vozol Vista (20.000 Puff)", 21000, "imagenes/Vozol Vista1.jpg", "descartables"],
-    ["Lost Maru (20.000 puff)", 26000, "imagenes/Lost Maru.jpg", "descartables"],
+    ["Lost Mary (20.000 puff)", 26000, "imagenes/Lost Maru.jpg", "descartables"],
     ["Black Sheep Duo (20.000 puff)", 29000, "imagenes/Black Sheep Duo1.jpg", "descartables"],
 
     ["Chris Brown (15.000 puff)", 19000, "imagenes/Chris Brown1.jpg", "descartables"],
 
-    ["Pone Pod (8.500 puff)", 18000, "imagenes/Pone Pod1.jpg", "descartables"],
+    ["Pyne Pod (8.500 puff)", 18000, "imagenes/Pone Pod1.jpg", "descartables"],
 
     ["Dummy x finity (6.900 puff)", 15000, "imagenes/Dummy x finity.jpg", "descartables"],
 
     ["Calibarn (6.000 puff)", 19000, "imagenes/Calibarn1.jpg", "descartables"],
 
     ["HDQ al 2% (5.000 puff)", 16000, "imagenes/HDQ al 2.jpg", "descartables"],
-    ["HDQ (5.000 puff)", 14000, "imagenes/HDQ.jpg", "descartables"],
 
     ["Zomo Party al 2% (4.500 puff)", 15000, "imagenes/Zomo Party al 2.jpg", "descartables"],
-    ["Zomo Party (4.500 puff)", 13000, "imagenes/Zomo Party.jpg", "descartables"],
 
     // Otros (no puffs, en gramos)
     ["Torch Cryo 7,5 gr", 58000, "imagenes/Torch Cryo 7,5 gr.jpg", "descartables"],
