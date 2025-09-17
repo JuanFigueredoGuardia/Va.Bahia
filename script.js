f817a7dd530e8e6b38980f5643846520918d0122
JuanFigueredoGuardia <juanfigueredoguardia@gmail.com>
2025-09-17 11:30:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,37 +2,45 @@ let carrito = [];
 let contador = document.getElementById('contador');
 let listaCarrito = document.getElementById('lista-carrito');
 let totalCarrito = document.getElementById('total-carrito');
-
 // Define las categorías de tus productos
 const productos = [
     // Descartables
-    ["Elfbar Nic King (40.000 puff)", 27000, "imagenes/Elfbar Nic King.jpg", "descartables"],
     ["Nikbar Ice baby (40.000 puff)", 29000, "imagenes/Nikbar ice baby.jpg", "descartables"],
+    ["Elfbar Ice Kings (40.000 Puff)", 29000, "imagenes/Elfbar Ice Kings.jpg", "descartables"],
+    ["Elfbar Nic king (40.000 puff)", 29000, "imagenes/Elfbar Nico king.jpg", "descartables"],
+    ["Ignite Ice (40.000 puff)", 30000, "imagenes/Ignite Ice1.jpg", "descartables"],
+
+    ["Black Sheep Dual (30.000 puff)", 35000, "imagenes/Black Sheep Dual.jpg", "descartables"],
+
     ["Ignite (25.000 puff)", 25000, "imagenes/Ignite .jpg", "descartables"],
+    ["Ignite gold (25.000 puff)", 27000, "imagenes/Ignite gold.jpg", "descartables"],
+    ["Frosty (25.000 puff)", 19500, "imagenes/Frosty1.jpg", "descartables"],
+
     ["Lost Mary (20.000 puff)", 23000, "imagenes/Lost Mary.jpg", "descartables"],
     ["Space Prism (20.000 puff)", 20000, "imagenes/Space Prism.jpg", "descartables"],
-    ["HDQ (5.000 puff)", 14000, "imagenes/HDQ.jpg", "descartables"],
-    ["Zomo Party (4.500 puff)", 13000, "imagenes/Zomo Party.jpg", "descartables"],
-    ["Elfbar Ice Kings (40.000 Puff)", 29000, "imagenes/Elfbar Ice Kings.jpg", "descartables"],
-    ["Elfbar Nico king (40.000 puff)", 29000, "imagenes/Elfbar Nico king.jpg", "descartables"],
-    ["Ignite Ice (40.000 puff)", 30000, "imagenes/Ignite Ice1.jpg", "descartables"],
-     ["Black Sheep Dual ( 30.000 puff)", 35000, "imagenes/Black Sheep Dual.jpg", "descartables"],
-    ["Ignite gold ( 25.000 puff)", 27000, "imagenes/Ignite gold.jpg", "descartables"],
-    ["Frosty (25.000 puff)", 19500, "imagenes/Frosty.jpg", "descartables"],
     ["Spaceman (20.000 puff)", 22000, "imagenes/Spaceman.jpg", "descartables"],
     ["Vozol Vista (20.000 Puff)", 21000, "imagenes/Vozol Vista1.jpg", "descartables"],
     ["Lost Maru (20.000 puff)", 26000, "imagenes/Lost Maru.jpg", "descartables"],
     ["Black Sheep Duo (20.000 puff)", 29000, "imagenes/Black Sheep Duo1.jpg", "descartables"],
+
     ["Chris Brown (15.000 puff)", 19000, "imagenes/Chris Brown1.jpg", "descartables"],
+
     ["Pone Pod (8.500 puff)", 18000, "imagenes/Pone Pod1.jpg", "descartables"],
+
     ["Dummy x finity (6.900 puff)", 15000, "imagenes/Dummy x finity.jpg", "descartables"],
+
     ["Calibarn (6.000 puff)", 19000, "imagenes/Calibarn1.jpg", "descartables"],
+
     ["HDQ al 2% (5.000 puff)", 16000, "imagenes/HDQ al 2.jpg", "descartables"],
+    ["HDQ (5.000 puff)", 14000, "imagenes/HDQ.jpg", "descartables"],
+
     ["Zomo Party al 2% (4.500 puff)", 15000, "imagenes/Zomo Party al 2.jpg", "descartables"],
+    ["Zomo Party (4.500 puff)", 13000, "imagenes/Zomo Party.jpg", "descartables"],
+
+    // Otros (no puffs, en gramos)
     ["Torch Cryo 7,5 gr", 58000, "imagenes/Torch Cryo 7,5 gr.jpg", "descartables"],
     ["Torch love Rozón 5 gr", 49000, "imagenes/Torch love Rozón 5 gr.jpg", "descartables"],
     ["Phenom Mushroom 6gr", 60000, "imagenes/Phenom Mushroom 6gr.jpg", "descartables"],
-    
 
     // Equipos
     ["Pod recargable zomo play", 19000, "imagenes/Pod recargable zomo play.jpg", "equipos"],
@@ -52,7 +60,7 @@ const productos = [
     ["Vidrios Smok Pen", 11000, "imagenes/Vidrios Smok Pen.jpg", "accesorios"],
     ["Bateria 21700", 18000, "imagenes/Bateria 21700.jpg", "accesorios"],
     ["Algodon Cotton Bacon 10gr", 8500, "imagenes/Algodon Cotton Bacon 10gr.jpg", "accesorios"],
-    ["Cargador Slim K2",  26.000, "imagenes/Cargador Slim K2.jpg", "accesorios"],
+    ["Cargador Slim K2",  26000, "imagenes/Cargador Slim K2.jpg", "accesorios"],
 ];
 
 // Abrir modal del carrito
